84fb2cb069abb3aad76dc085803af9afff7a0c83
CarlosCSCJ <csaenzadan@jesuitasformacion.com>
2020-01-31 08:27:32 +0100

    fist commit maven

---
+++ src/main/java/es/unirioja/uml2prov/bgm/BGMEventListener.java
@@ -0,0 +1,11 @@
+package es.unirioja.uml2prov.bgm;
+
+public interface BGMEventListener {
+	
+	public void newValueBinding(BGMEvent e);
+	public void newBinding(BGMEvent e);
+	public void operationStart(BGMEvent e);
+	public void operationEnd(BGMEvent e);
+}
+
+
